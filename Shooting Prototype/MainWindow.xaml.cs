d5e381f8d9345a1f63720ed2fd65021f7b56cd94
JackSnakeMGS4 <jaimerivas81@yahoo.com>
2018-03-29 11:26:20 -0700

    Really close to finishing the prototype. Just having difficulty dealing with shots not behaving as intended.

--- Shooting Prototype/MainWindow.xaml.cs
+++ Shooting Prototype/MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace Shooting_Prototype
         private int projectileHeight = 20;//for projectile's height
         private double projectileCenterX;//for projectile x-axis center
         private double projectileY;//for projectil bottom most point
-        private int projectileSpeed = 10;//rate at which projectile moves
+        private int projectileSpeedY = 10;//rate at which projectile moves
         private Rectangle projectile;//let's me reference the projectile outside of the drawing function
 
         public MainWindow()
@@ -53,9 +53,10 @@ namespace Shooting_Prototype
 
         private void Update_Tick(object sender, EventArgs e)
         {       
-            MoveAll();
-            if (Keyboard.IsKeyDown(Key.V))
+            MoveAll();//calling MoveAll() first so projectile are actually fired from the player's current position
+            if (Keyboard.IsKeyDown(Key.V))//is fired when V is pressed or held down
             {
+                //running into an with the shots freezing in mid air if V is pressed multiple times or held down
                 DrawProjectiles(projectileWidth, projectileHeight, Colors.Firebrick, Colors.White, 2,
                     playerCenterX - (projectileWidth / 2)/*sets location of shot's center to the center of the player*/,
                     playerTopY/*sets the location of the shot's bottom point*/);
@@ -65,22 +66,25 @@ namespace Shooting_Prototype
         private void MoveAll()
         {
             playerCenterX = playerX + (player.Width / 2);//player's x-axis center is 200
-            if (projectile != null)
-            {               
-                projectileCenterX = Canvas.GetLeft(projectile);
-                projectileY = Canvas.GetBottom(projectile);
 
-                projectileY += projectileSpeed;
+            if (projectile != null)//fires only if projectile isn't null
+            {               
+                projectileCenterX = Canvas.GetLeft(projectile);//get the updated x-coord for the projectile
+                projectileY = Canvas.GetBottom(projectile);//get the updated y-coord for the projectile which is always the same since the player doesn't move up or down
+
+                projectileY += projectileSpeedY;//adds projectileSpeed to projectileY
                
-                Canvas.SetBottom(projectile, projectileY);
-                projectile.Visibility = Visibility.Visible;
+                Canvas.SetBottom(projectile, projectileY);//sets projectile's bottom to new projectileY
+                projectile.Visibility = Visibility.Visible;//makes projectile visible after it's set
             }
            
+            //next line checks if projectile bottom is past the play area height
             if (projectileY > gameCanvas.Height)
             {
-                projectile = null;
+                projectile = null;//makes projectile null if it goes off the top of the play area
             }
 
+            //next two if statements are for the player movement on the x-axis and will fire if the player is within the game boundaries
             if (Keyboard.IsKeyDown(Key.A) && playerX > 0)
             {
                 playerX -= playerSpeedX;
@@ -95,6 +99,20 @@ namespace Shooting_Prototype
 
         private void DrawProjectiles(int shotWidth, int shotHeight, Color shotOutline, Color shotColor, int shotThickness, double xAxisLocation, double yAxisLocation)
         {
+            /*creates new instance of a rectangle named projectile
+             * hides the shot then sets it width/height
+             * sets outline color, thickness of outline, and inner color
+             * sets shot's position based on the updated player's x and y coords
+             */
+            /*Note to self: Issue is occuring somewhere here in conjunction with the 
+             * event handler for the V key because the variable refers to only one object at any given time
+             * needs to find a way have multiple shots individually based off one reference 
+             * probably not possible and will most likely need to have a list of shots
+             * something like this: List<Rectangle> shotsList = new List<Rectangle>();
+             * That way I can added multiple objects to it and loop through the list and fire
+             * the ones that in the list and then I should remove them from the list once they go
+             * off screen
+             */
             projectile = new Rectangle();
             projectile.Visibility = Visibility.Hidden;
             projectile.Width = projectileWidth;
